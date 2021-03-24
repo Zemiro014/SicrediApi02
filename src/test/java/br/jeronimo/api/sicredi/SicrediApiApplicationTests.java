77cde14fb7e1992240c60a8c00f1592a364c3214
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-24 14:33:19 -0300

    First commit (API configuration)

---
+++ src/test/java/br/jeronimo/api/sicredi/SicrediApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jeronimo.api.sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
